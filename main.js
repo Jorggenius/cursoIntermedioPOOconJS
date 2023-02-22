fb6d317015378a33921cb7159b8321e5cbb81a84
jorge luis <martinezmonajorgeluis@gmail.com>
2023-02-22 18:01:15 -0500

    Este es el commit respectivo a la clase # 6 (shallow copy con JS) del curso intermedio de POO con JS de Platzi

--- main.js
+++ main.js
@@ -1,41 +1,10 @@
-const juan = {
-    name: "Juanito",
-    age: 18,
-    approvedCourses: ["curso 1"],
-    addCourses(newCourse){
-        console.log("This", this)
-        console.log("this.approvedcourses", this.approvedCourses)
-        this.approvedCourses.push(newCourse)
-    }
+const obj1 = {
+    a: "a",
+    b: "b",
 }
 
-// Object.keys(juan)
-// Object.getOwnPropertyNames(juan)
-// Object.entries(juan)
+const obj2 = {}
 
-Object.defineProperties(juan, "Navigator", {
-    value: "chrome",
-    enumerable: false,
-    writable: true,
-    configurable: true,
-})
-Object.defineProperties(juan, "editor", {
-    value: "VScode",
-    enumerable: true,
-    writable: false,
-    configurable: true,
-})
-Object.defineProperties(juan, "terminal", {
-    value: "WSL",
-    enumerable: true,
-    writable: true,
-    configurable: false,
-})
-Object.defineProperties(juan, "pruebaNasa", {
-    value: "extraterrestres",
-    enumerable: false,
-    writable: false,
-    configurable: false,
-})
-
-Object.getOwnPropertyDescriptors(juan)
+for(prop in obj1){
+obj2[prop]
+}
