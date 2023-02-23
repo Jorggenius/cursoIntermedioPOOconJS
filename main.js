26bb8f890e852cc07b6a44b38e422457a484047d
jorge luis <martinezmonajorgeluis@gmail.com>
2023-02-23 17:40:03 -0500

    Este es el comit respectivo a la class # 8 ( Que es la recursividad) de el curso intermedio de POO con JS de Plati

--- main.js
+++ main.js
@@ -1,15 +1,40 @@
-const obj1 = {
-    a: "a",
-    b: "b",
-    c: {
-      d: "d",
-      e: "e",  
-    },
-    editA(){
-        this.a = "AAAAAA";
-    }
+// const obj1 = {
+//     a: "a",
+//     b: "b",
+//     c: {
+//       d: "d",
+//       e: "e",  
+//     },
+//     editA(){
+//         this.a = "AAAAAA";
+//     }
+// }
+
+// const stringifiedComplexObj = JSON.stringify(obj1)
+// const obj2 = JSON.parse(stringifiedComplexObj)
+
+// function recursiva(){
+//   if(/*validacion */){
+//     //llamados recurcibos
+//   }else{
+//     //llamados normales, sin recursividad
+//   }
+// }
+
+const numeritos =[0,2,3,4,5,5,6,7,8,9,10,11,12,13]
+
+// let numerito = 0
+
+// for(let index = 0; index < numeritos.length; i++){
+//   numerito = numeritos[index]
+//   console.log({index, numerito})
+// }
+
+function recursiva(numbersArray){
+  if(numbersArray.length != 0){
+    const firstNum = numbersArray[0]
+    console.log(firstNum)
+    numbersArray.shift()
+    recursiva(numbersArray)
+  }
 }
-
-const stringifiedComplexObj = JSON.stringify(obj1)
-const obj2 = JSON.parse(stringifiedComplexObj)
-
